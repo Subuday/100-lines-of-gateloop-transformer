150faa6bfbe50852bbc63cb7d5f211b9851805a7
Subuday <mr.gigant977@gmail.com>
2024-02-24 21:25:18 +0000

    Add gateloop attention backbone

--- gateloop_transformer.py
+++ gateloop_transformer.py
@@ -1,6 +1,7 @@
 import torch
 from torch import nn
 from torch.nn import Module, ModuleList
+from einops import einsum, rearrange
 
 class RMSNorm(Module):
 
@@ -12,11 +13,41 @@ class RMSNorm(Module):
 
 class GateLoopAttention(Module):
 
-    def __init__(self):
+    def __init__(
+        self,
+        dim: int,
+        *,
+        dim_inner: int,
+        num_heads: int,
+    ):
         super().__init__()
+        self.dim_inner = dim_inner
+        self.num_heads = num_heads
+        assert (dim_inner % num_heads) == 0, f'dimension {dim_inner} must be divisible by number of gate loop heads {num_heads}'
+
+        self.to_qkv = nn.Linear(dim, dim_inner * 3, bias = False)
+        self.to_a = nn.Linear(dim, num_heads * 2)
+
+        self.out_proj = nn.Linear(dim_inner, dim, bias = False) if dim_inner != dim else nn.Identity()
 
     def forward(self, x):
-        return x
+        q, k, v = self.to_qkv(x).chunk(3, dim = -1)
+        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> (b h) n d', h = self.num_heads), (q, k, v))
+
+        a = self.to_a(x)
+        a = rearrange(a, 'b n (h c) -> (b h) n 1 1 c', h = self.num_heads, c = 2)
+        a = torch.view_as_complex(a)
+
+        magnitude, phase = a.abs(), a.angle()
+        a = torch.polar(magnitude.sigmoid(), phase)
+
+        out = self._gate_loop(q, k, v, a)
+        out = rearrange(out, '(b h) n d -> b n (h d)', h = self.num_heads)
+        return self.out_proj(out)
+
+    @staticmethod
+    def _gate_loop(q, k, v, a):
+        return k
     
 class FeedForward(Module):
 
@@ -42,7 +73,11 @@ class Transformer(Module):
             self.layers.append(
                 ModuleList([
                     RMSNorm(),
-                    GateLoopAttention(),
+                    GateLoopAttention(
+                        dim,
+                        dim_inner = 256,
+                        num_heads = 4
+                    ),
                     RMSNorm(),
                     FeedForward()
                 ])
